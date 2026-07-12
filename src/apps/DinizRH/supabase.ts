bbd4587ac647a102a43b06488fd751a6c1ebc0ae
Danilo <danilo@dinizrh.local>
2026-07-12 10:23:11 -0300

    Corrige Supabase na Vercel

--- src/apps/DinizRH/supabase.ts
+++ src/apps/DinizRH/supabase.ts
@@ -2,8 +2,13 @@ import {
   createClient,
 } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL ||
+  "https://mvbhenupgghvludwcboy.supabase.co";
+
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY ||
+  "sb_publishable_6BaGDTWr2ynp0Xmk3ei6AQ_T7GgLi5H";
 
 export const supabase = createClient(
   supabaseUrl,
